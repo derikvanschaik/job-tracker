c54cee1c9f1676b5d26efb769c589d4487013b1d
Derik van Schaik <derikvanschaik@gmail.com>
2024-05-11 13:45:18 -0700

    start deploy workflow file

--- main_test.ts
+++ main_test.ts
@@ -4,9 +4,11 @@ import { assertEquals } from "https://deno.land/std/assert/mod.ts";
 
 const app = createApp();
 
+const API_URL = Deno.env.get("API_URL") || "";
+
 Deno.test("Hello World", async () => {
   app.get("/", (c) => c.text("Please test me"));
 
-  const res = await app.request("http://localhost/");
+  const res = await app.request(API_URL);
   assertEquals(res.status, 200);
 });
